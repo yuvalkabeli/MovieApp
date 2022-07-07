afff71c7da94098c82b3d2d06ec9ba149e399a40
yuvalkabeli <yuvalkabeli@gmail.com>
2022-07-07 17:45:53 +0300

    added fetch feature
    I fetched the movies data from the api

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -20,21 +20,30 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import Section from './components/Section'
-
+// import getMovies from './helpers/helpers';
 //const App: () => Node = () => {
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
+  const [movies, setMovies] = useState([])
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch('http://10.0.2.2:3000/getMovies')
+      response = await response.json()
+      setMovies([...response])
+    }
+
+    fetchMyAPI()
+  }, [])
+
+
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -51,13 +60,13 @@ const App = () => {
             information about the movie
           </Section>
           <Section title="Movie Title2">
-            information about the movie
+            information about the mo
           </Section>
           <Section title="Movie Title3">
-            information about the movie
+            {movies[0].rating}
           </Section>
           <Section title="Movie Title4">
-            information about the movie
+            Read the docs to discover what to do next
           </Section>
         </View>
       </ScrollView>
---
+++ AwesomeProject/components/Movies.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View } from 'react-native'
+import Section from './Section'
+
+export default function Movies({ moviesArray }) {
+
+    const makeMovies = () => {
+        return moviesArray.map
+    }
+
+    return (
+        <View>
+
+        </View>
+    )
+}
